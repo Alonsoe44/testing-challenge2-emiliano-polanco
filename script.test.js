c5de824256c09c1b3a39d97606043d5e3c83a16b
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-01-18 22:38:43 +0100

    clean strictEquals function and leggible

--- script.test.js
+++ script.test.js
@@ -1,13 +1,9 @@
 const strictEquals = (a, b) => {
-    if (Object.is(a, -0)) {
-        if (Object.is(b, 0)) {
-            return true;
-        }
-    }
-    if (Object.is(b, -0)) {
-        if (Object.is(a, 0)) {
-            return true;
-        }
+    if (
+        (Object.is(a, -0) && Object.is(b, 0)) ||
+        (Object.is(b, -0) && Object.is(a, 0))
+    ) {
+        return true;
     }
     if (Number.isNaN(a) && Number.isNaN(b)) {
         return false;
